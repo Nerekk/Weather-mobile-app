86ff0cfb6444ba22e2a52664869b380691d9fdf4
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-04-16 16:03:07 +0200

    Basic viewpager2 implementation

--- app/src/main/java/com/example/weather_mobile_app/MainActivity.java
+++ app/src/main/java/com/example/weather_mobile_app/MainActivity.java
@@ -7,18 +7,64 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends FragmentActivity {
+
+    private ViewPager2 viewPager;
+    private BottomNavigationView bottomNavigationView;
+    private FragmentStateAdapter pagerAdapter;
+    private List<Fragment> fragmentList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        createFragmentList();
+
+        // Instantiate a ViewPager2 and a PagerAdapter.
+        viewPager = findViewById(R.id.pager);
+        pagerAdapter = new ScreenSlidePagerAdapter(this, fragmentList);
+        viewPager.setAdapter(pagerAdapter);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                bottomNavigationView.getMenu().getItem(position).setChecked(true);
+            }
         });
     }
+
+    private void createFragmentList() {
+        fragmentList = new ArrayList<>();
+        fragmentList.add(new ScreenSlideFragment(R.layout.fragment_weather_main));
+        fragmentList.add(new ScreenSlideFragment(R.layout.fragment_favourites_main));
+        fragmentList.add(new ScreenSlideFragment(R.layout.fragment_settings_main));
+    }
+
+    // obsluga stosu
+//    @Override
+//    public void onBackPressed() {
+//        if (viewPager.getCurrentItem() == 0) {
+//            // If the user is currently looking at the first step, allow the system to handle the
+//            // Back button. This calls finish() on this activity and pops the back stack.
+//            super.onBackPressed();
+//        } else {
+//            // Otherwise, select the previous step.
+//            viewPager.setCurrentItem(viewPager.getCurrentItem() - 1);
+//        }
+//    }
+
+
 }
---
+++ app/src/main/java/com/example/weather_mobile_app/ScreenSlidePagerAdapter.java
@@ -0,0 +1,36 @@
+package com.example.weather_mobile_app;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ScreenSlidePagerAdapter extends FragmentStateAdapter {
+
+    List<Fragment> fragmentList;
+
+    public ScreenSlidePagerAdapter(FragmentActivity fa, List<Fragment> fragmentList) {
+        super(fa);
+        this.fragmentList = fragmentList;
+    }
+
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return fragmentList.get(0);
+            case 1:
+                return fragmentList.get(1);
+            case 2:
+                return fragmentList.get(2);
+        }
+        return null;
+    }
+
+    @Override
+    public int getItemCount() {
+        return fragmentList.size();
+    }
+}
