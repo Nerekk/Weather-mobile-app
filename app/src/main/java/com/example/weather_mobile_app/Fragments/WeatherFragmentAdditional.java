53a9178f543f6a3b572e1aad15554addc1a39bbf
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-04-17 22:14:51 +0200

    Weather fragments basic implementation

---
+++ app/src/main/java/com/example/weather_mobile_app/Fragments/WeatherFragmentAdditional.java
@@ -0,0 +1,26 @@
+package com.example.weather_mobile_app.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.weather_mobile_app.R;
+
+public class WeatherFragmentAdditional extends Fragment {
+
+    public WeatherFragmentAdditional() {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_weather2, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
