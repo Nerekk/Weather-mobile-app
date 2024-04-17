1654f3fa8959625b714c1fd623e3cee9f97fc1f4
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-04-17 22:13:56 +0200

    Weather recycler deletion

--- app/src/main/java/com/example/weather_mobile_app/Fragments/ScreenSlideFragment.java
+++ app/src/main/java/com/example/weather_mobile_app/Fragments/ScreenSlideFragment.java
@@ -10,19 +10,14 @@ import android.widget.ImageView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.weather_mobile_app.Adapters.WeatherRecyclerAdapter;
 import com.example.weather_mobile_app.R;
 
 public class ScreenSlideFragment extends Fragment {
 
     private int layoutId;
-    private RecyclerView recyclerView;
-    private WeatherRecyclerAdapter weatherRecyclerAdapter;
-    private boolean isWeather;
 
-    public ScreenSlideFragment(int id, boolean isWeather) {
+    public ScreenSlideFragment(int id) {
         this.layoutId = id;
-        this.isWeather = isWeather;
     }
 
     @Override
@@ -34,26 +29,6 @@ public class ScreenSlideFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Log.i("onViewCreated", "START");
-        if (isWeather) {
-            Log.i("onViewCreated", "IF");
-            String[] data = new String[10];
-            data[0] = "Pierwszy";
-            data[1] = "Drugi";
-            data[2] = "Trzeci";
-            data[3] = "Czwarty";
-            data[4] = "Piąty";
-            data[5] = "Szosty";
-            data[6] = "Siodmy";
-            data[7] = "Osmy";
-            data[8] = "Dziewiaty";
-            data[9] = "Dziesiaty";
-            weatherRecyclerAdapter = new WeatherRecyclerAdapter(data);
-            recyclerView = view.findViewById(R.id.recyclerWeather);
-            Log.i("onViewCreated", "ADAPTER");
-            recyclerView.setAdapter(weatherRecyclerAdapter);
-        }
-        Log.i("onViewCreated", "END");
     }
 
 }
