a37a88cc5241902ef754b444be2d9d8714068659
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-04-22 22:55:53 +0200

    Get forecast weather method addition

--- app/src/main/java/com/example/weather_mobile_app/Interfaces/RequestWeatherService.java
+++ app/src/main/java/com/example/weather_mobile_app/Interfaces/RequestWeatherService.java
@@ -1,6 +1,7 @@
 package com.example.weather_mobile_app.Interfaces;
 
 import com.example.weather_mobile_app.WeatherAPI.Models.Current.CurrentWeatherData;
+import com.example.weather_mobile_app.WeatherAPI.Models.Forecast.ForecastWeatherData;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -9,4 +10,7 @@ import retrofit2.http.Query;
 public interface RequestWeatherService {
     @GET("weather?appid=95e84a749af41c408381fbc6f39a0e0a&units=metric")
     Call<CurrentWeatherData> getCurrentWeather(@Query("q") String city);
+
+    @GET("forecast?appid=95e84a749af41c408381fbc6f39a0e0a&units=metric")
+    Call<ForecastWeatherData> getForecastWeather(@Query("q") String city);
 }
