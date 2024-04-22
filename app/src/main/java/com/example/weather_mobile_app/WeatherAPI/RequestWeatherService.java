f5fcb0d63844394190d1adfeccd7f807d900cbc1
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-04-22 20:19:41 +0200

    RequestWeatherService refactoring

--- app/src/main/java/com/example/weather_mobile_app/WeatherAPI/RequestWeatherService.java
+++ app/src/main/java/com/example/weather_mobile_app/WeatherAPI/RequestWeatherService.java
@@ -1,14 +1,12 @@
 package com.example.weather_mobile_app.WeatherAPI;
 
+import com.example.weather_mobile_app.WeatherAPI.Models.Current.CurrentWeatherData;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface RequestWeatherService {
-//    @GET("data/2.5/weather?q={city}&units=metric&appid=95e84a749af41c408381fbc6f39a0e0a")
-//    Call<CurrentWeatherData> getCurrentWeather(@Path("city") String city);
-
     @GET("weather?appid=95e84a749af41c408381fbc6f39a0e0a&units=metric")
     Call<CurrentWeatherData> getCurrentWeather(@Query("q") String city);
 }
