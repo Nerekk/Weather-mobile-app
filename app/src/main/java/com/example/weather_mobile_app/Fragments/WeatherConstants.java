5055fac214cb11e4877ad781433de3b7d91aa570
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-04-22 20:21:52 +0200

    WeatherConstants class addition

---
+++ app/src/main/java/com/example/weather_mobile_app/Fragments/WeatherConstants.java
@@ -0,0 +1,6 @@
+package com.example.weather_mobile_app.Fragments;
+
+public class WeatherConstants {
+    public static final String DEGREES = "°";
+    public static final String DEGREES_CELSIUS = "°C";
+}
