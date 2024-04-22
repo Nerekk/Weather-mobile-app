7a6adc029536f4c3cbcda57bf629ccdc6f75f75c
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-04-22 20:22:12 +0200

    updateApi method implementation

--- app/src/main/java/com/example/weather_mobile_app/Adapters/ScreenSlidePagerAdapter.java
+++ app/src/main/java/com/example/weather_mobile_app/Adapters/ScreenSlidePagerAdapter.java
@@ -1,15 +1,19 @@
 package com.example.weather_mobile_app.Adapters;
 
+import android.util.Log;
+
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
-import java.util.ArrayList;
+import com.example.weather_mobile_app.Fragments.WeatherFragmentBasic;
+import com.example.weather_mobile_app.WeatherAPI.Models.Current.CurrentWeatherData;
+
 import java.util.List;
 
 public class ScreenSlidePagerAdapter extends FragmentStateAdapter {
 
-    List<Fragment> fragmentList;
+    private List<Fragment> fragmentList;
 
     public ScreenSlidePagerAdapter(FragmentActivity fa, List<Fragment> fragmentList) {
         super(fa);
@@ -32,5 +36,12 @@ public class ScreenSlidePagerAdapter extends FragmentStateAdapter {
     @Override
     public int getItemCount() {
         return fragmentList.size();
+    }
+
+    public void updateApi(CurrentWeatherData data) {
+        Fragment weather = fragmentList.get(0);
+        WeatherFragmentBasic weatherBasic = (WeatherFragmentBasic) weather.getChildFragmentManager().findFragmentByTag("basicFragment");
+        Log.i("FRAGMENT", String.valueOf(weatherBasic==null));
+        weatherBasic.updateData(data);
     }
 }
