63cf5f706d0dc697293c81f8d2b1eba7ad6e4fb3
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-04-24 13:05:35 +0200

    Deletion of WeatherConstants

--- app/src/main/java/com/example/weather_mobile_app/Fragments/WeatherConstants.java
+++
@@ -1,6 +0,0 @@
-package com.example.weather_mobile_app.Fragments;
-
-public class WeatherConstants {
-    public static final String DEGREES = "°";
-    public static final String DEGREES_CELSIUS = "°C";
-}
