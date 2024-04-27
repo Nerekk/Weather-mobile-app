2d2455b3ab059543044b4cbbfb2d05208338ccdc
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-04-28 00:46:12 +0200

    New unit selector look + kelvins

--- app/src/main/java/com/example/weather_mobile_app/AppConfig.java
+++ app/src/main/java/com/example/weather_mobile_app/AppConfig.java
@@ -2,17 +2,24 @@ package com.example.weather_mobile_app;
 
 public class AppConfig {
     public static boolean isRefreshSwitchEnabled;
-    public static boolean isUnitsSwitchEnabled;
+
+    public static final int UNIT_D = 0;
+    public static final int UNIT_M = 1;
+    public static final int UNIT_I = 2;
+
+    private static int unitsIndex = UNIT_D;
 
     public static String currentLoc;
 
-    public static final String IMPERIAL = "imperial";
-    public static final String METRIC = "metric";
     public static final String DEFAULT = "default";
+    public static final String METRIC = "metric";
+    public static final String IMPERIAL = "imperial";
+
     public static final String DEGREES = "°";
+
+    public static final String DEGREES_KELVIN = "°K";
     public static final String DEGREES_CELSIUS = "°C";
     public static final String DEGREES_FAHRENHEIT = "°F";
-    public static final String DEGREES_KELVIN = "°K";
 
     public static int getRefreshTime() {
         if (isRefreshSwitchEnabled) {
@@ -23,19 +30,39 @@ public class AppConfig {
     }
 
     public static String getUnitsType() {
-        if (isUnitsSwitchEnabled) {
-            return IMPERIAL;
-        } else {
-            return METRIC;
+        switch (unitsIndex) {
+            case UNIT_D:
+                return DEFAULT;
+            case UNIT_M:
+                return METRIC;
+            case UNIT_I:
+                return IMPERIAL;
         }
+        return null;
     }
 
     public static String getDegreesType() {
-        if (isUnitsSwitchEnabled) {
-            return DEGREES_FAHRENHEIT;
-        } else {
-            return DEGREES_CELSIUS;
+        switch (unitsIndex) {
+            case UNIT_D:
+                return DEGREES_KELVIN;
+            case UNIT_M:
+                return DEGREES_CELSIUS;
+            case UNIT_I:
+                return DEGREES_FAHRENHEIT;
         }
+        return null;
+    }
+
+    public static void setUnitsIndex(int unitsIndex) {
+        if (unitsIndex < 0 || unitsIndex > 2) {
+            AppConfig.unitsIndex = 0;
+        } else {
+            AppConfig.unitsIndex = unitsIndex;
+        }
+    }
+
+    public static int getUnitsIndex() {
+        return unitsIndex;
     }
 
     public static String getCurrentLoc() {
--- app/src/main/java/com/example/weather_mobile_app/Fragments/SettingsFragment.java
+++ app/src/main/java/com/example/weather_mobile_app/Fragments/SettingsFragment.java
@@ -1,12 +1,17 @@
 package com.example.weather_mobile_app.Fragments;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -14,7 +19,14 @@ import com.example.weather_mobile_app.AppConfig;
 import com.example.weather_mobile_app.MainActivity;
 import com.example.weather_mobile_app.R;
 
+import java.util.ArrayList;
+
 public class SettingsFragment extends Fragment {
+    private static ArrayList<String> unitList;
+    private static ArrayAdapter<String> adapter;
+    private static ListView units;
+    private static TextView tvUnits;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings_main, container, false);
@@ -24,6 +36,8 @@ public class SettingsFragment extends Fragment {
     }
 
     private static void prepareComponents(View view) {
+        tvUnits = view.findViewById(R.id.tvUnits);
+
         ImageView iv = (ImageView) view.findViewById(R.id.ivRefresh);
         iv.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,12 +54,29 @@ public class SettingsFragment extends Fragment {
             }
         });
 
-        Switch unitSwitch = (Switch) view.findViewById(R.id.unitSwitch);
-        unitSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        unitList = new ArrayList<>();
+        unitList.add("Default (Kelvin)");
+        unitList.add("Metric (Celsius)");
+        unitList.add("Imperial (Fahrenheit)");
+
+        units = (ListView) view.findViewById(R.id.lvUnits);
+        adapter = new ArrayAdapter<>(MainActivity.getMainActivity(), android.R.layout.simple_list_item_1, unitList);
+        units.setAdapter(adapter);
+
+        updateUnitInfo();
+        units.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                AppConfig.isUnitsSwitchEnabled = b;
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                AppConfig.setUnitsIndex(position);
+                MainActivity.getMainActivity().getAPIData();
+                updateUnitInfo();
+                MainActivity.writeToast("Units set: " + unitList.get(position));
             }
         });
+    }
+
+    private static void updateUnitInfo() {
+        String unit = "Units: " + AppConfig.getUnitsType();
+        tvUnits.setText(unit);
     }
 }
