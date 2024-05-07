699711bdbffb247cb558a5c5a27bc07083e1ebdc
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-05-07 22:16:30 +0200

    Addition of json identificators

--- app/src/main/java/com/example/weather_mobile_app/WeatherAPI/Models/Current/CurrentWeatherJsonHolder.java
+++ app/src/main/java/com/example/weather_mobile_app/WeatherAPI/Models/Current/CurrentWeatherJsonHolder.java
@@ -1,16 +1,37 @@
 package com.example.weather_mobile_app.WeatherAPI.Models.Current;
 
 public class CurrentWeatherJsonHolder {
+    public static final String NAME = "name";
     private String name;
+
+    public static final String COORDS = "coords";
     private String coords;
+
+    public static final String DATE = "date";
     private String date;
+
+    public static final String ICON = "icon";
     private String icon;
+
+    public static final String DESC = "desc";
     private String desc;
+
+    public static final String TEMP = "temp";
     private Integer temp;
+
+    public static final String WIND_DEGREE = "windDegree";
     private Integer windDegree;
+
+    public static final String WIND = "wind";
     private Double wind;
+
+    public static final String HUMIDITY = "humidity";
     private String humidity;
+
+    public static final String VISIBILITY = "visibility";
     private Integer visibility;
+
+    public static final String PRESSURE = "pressure";
     private String pressure;
 
     public CurrentWeatherJsonHolder(String name, String coords, String date, String icon, String desc, Integer temp, Integer windDegree, Double wind, String humidity, Integer visibility, String pressure) {
